10145f7b8f33e9154ed52762e3465ee5c6abe9da
Josh Rendek <jrendek@octolabs.io>
2018-09-02 17:26:42 -0400

    Work on statsd

--- stats/stats.go
+++ stats/stats.go
@@ -5,29 +5,25 @@ import (
 	//"github.com/rs/zerolog/log"
 	//"gopkg.in/alexcesaro/statsd.v2"
 	//"gopkg.in/alexcesaro/statsd.v2"
+	//"gopkg.in/alexcesaro/statsd.v2"
+
+	"github.com/quipo/statsd"
 	"os"
 )
 
 var (
 	StatsdHost string
-	//c          *statsd.Client
-	logger = zerolog.New(os.Stdout).With().Caller().Str("stats", "").Logger()
+	c          *statsd.StatsdClient
+	logger     = zerolog.New(os.Stdout).With().Caller().Str("stats", "").Logger()
 )
 
 func Setup() {
-	//var err error
-	//c, err = statsd.New(statsd.Address(StatsdHost))
-	//if err != nil {
-	//	// If nothing is listening on the target port, an error is returned and
-	//	// the returned client does nothing but is still usable. So we can
-	//	// just log the error and go on.
-	//	log.Print(err)
-	//}
+	c = statsd.NewStatsdClient(StatsdHost, "honeypot")
 	logger.Print("statsd host: ", StatsdHost)
 }
 
 func Increment(key string) {
-	//c.Increment("honeypot." + key)
+	c.Incr(key, 1)
 }
 
 func Timing(bucket string) {
